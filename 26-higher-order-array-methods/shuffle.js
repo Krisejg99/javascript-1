43ae777dce85fa8f7e1ec1c53ab715e4a4d9f706
Kristopher Gray <kristopher.gray99@gmail.com>
2022-11-22 17:28:02 +0100

    shuffle, FisherYates random nr fnc

---
+++ 26-higher-order-array-methods/shuffle.js
@@ -0,0 +1,34 @@
+/**
+ * Higher Order Array Methods
+ *
+ * .sort()
+ * .filter()
+ * .find()
+ * .map()
+ * .reduce()
+ *
+ * - Clone an array
+ * - Shuffle an array <--
+ */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+// // console.log("Numbers before shuffling:", numbers);
+
+// numbers.sort(() => {
+//     return 0.5 - Math.random();
+// });
+
+// console.log(numbers);
+
+// FisherYates random number function
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+shuffleArray(numbers);
+console.log("Numbers after proper shuffling:", numbers);
