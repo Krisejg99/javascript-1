fd33438efd3b531bf2032c5670364351c7dc8c79
Kristopher Gray <kristopher.gray99@gmail.com>
2022-12-08 12:03:03 +0100

    added 38-typescript. Introduction

---
+++ 38-typescript/script.js
@@ -0,0 +1,10 @@
+/**
+ * TypeScript
+ *
+ */
+var username = 'Krise';
+var x;
+x = 10;
+// x = 'hej';
+var names = [];
+console.log(names);
---
+++ 38-typescript/script.ts
@@ -0,0 +1,14 @@
+/**
+ * TypeScript
+ *
+ */
+
+let username = 'Krise';
+
+let x: number;
+
+x = 10;
+// x = 'hej';
+
+let names: string[] = [];
+console.log(names);
