41ff9fd2df110f9603c7d6a830a98dad81763503
Kristopher Gray <kristopher.gray99@gmail.com>
2022-12-09 12:03:48 +0100

    fetchTodos, getTodos, renderTodos done

--- 40-async-todos-ts/frontend/src/main.ts
+++ 40-async-todos-ts/frontend/src/main.ts
@@ -1,2 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
+
+interface ITodo {
+	id: number,
+	title: string,
+	completed: boolean
+}
+
+let todos: ITodo[] = []
+
+
+
+const fetchTodos = async () => {
+	const res = await fetch('http://localhost:3001/todos')
+	if (!res.ok) throw new Error(`Could't fetch data, no respornse:, ${res.status} ${res.statusText}`)
+	return await res.json() as ITodo[]
+}
+
+const getTodos = async () => {
+	todos = await fetchTodos()
+	renderTodos()
+}
+
+const renderTodos = () => {
+	const todoListEl = document.querySelector('#todos')!
+	todoListEl.innerHTML = ''
+	todoListEl.innerHTML = todos
+		.map(todo => `<li class="list-group-item ${todo.completed ? 'completed' : ''}">${todo.title}</li>`)
+		.join('')
+}
+
+getTodos()
