9bc857d5acb9be9083caf018ee2062adf709abfe
Kristopher Gray <kristopher.gray99@gmail.com>
2022-12-09 15:38:49 +0100

    fetch POST newTodo to server

--- 40-async-todos-ts/frontend/src/main.ts
+++ 40-async-todos-ts/frontend/src/main.ts
@@ -1,19 +1,48 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
 
+
+/*************************************************************************************
+ * INTERFACES
+ */
+
 interface ITodo {
-	id: number,
+	id?: number,
 	title: string,
 	completed: boolean
 }
 
+
+/*************************************************************************************
+ * VARIABLES
+ */
+
+
 let todos: ITodo[] = []
 
 
+/*************************************************************************************
+ * FUNCTIONS
+ */
+
+
+const checkRes = (res: Response) => {
+	if (!res.ok) throw new Error(`Could't fetch data, no respornse:, ${res.status} ${res.statusText}`)
+}
+
+const createNewTodo = async (newTodo: ITodo) => {
+	const res = await fetch('http://localhost:3001/todos', {
+		method: 'POST',
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify(newTodo),
+	})
+
+	checkRes(res)
+}
 
 const fetchTodos = async () => {
 	const res = await fetch('http://localhost:3001/todos')
-	if (!res.ok) throw new Error(`Could't fetch data, no respornse:, ${res.status} ${res.statusText}`)
+	checkRes(res)
 	return await res.json() as ITodo[]
 }
 
@@ -24,10 +53,56 @@ const getTodos = async () => {
 
 const renderTodos = () => {
 	const todoListEl = document.querySelector('#todos')!
-	todoListEl.innerHTML = ''
-	todoListEl.innerHTML = todos
-		.map(todo => `<li class="list-group-item ${todo.completed ? 'completed' : ''}">${todo.title}</li>`)
-		.join('')
+	const completedTodoListEl = document.querySelector('#completed-todos')!
+
+	const refreshTodos = (el: Element, boo: Boolean) => {
+		el.innerHTML = ''
+		el.innerHTML = todos
+			.filter(todo => todo.completed === boo)
+			.map(todo => `
+				<li class="list-group-item data-todo-id="${todo.id}">
+					${todo.title}
+				</li>`)
+			.join('')
+	}
+	refreshTodos(todoListEl, false)
+	refreshTodos(completedTodoListEl, true)
+
 }
+
+
+/*************************************************************************************
+ * EVENT LISTENERS
+ */
+
+
+document.querySelector('#new-todo-form')?.addEventListener('submit', async e => {
+	e.preventDefault()
+
+	const newTodoEl = document.querySelector('#newTodo') as HTMLInputElement
+
+	if (!newTodoEl.value) { 
+		alert("That's not an item")
+		return
+	}
+
+	const newTodoTitle = newTodoEl.value
+	newTodoEl.value = ''
+
+	const newTodo: ITodo = {
+		title: newTodoTitle,
+		completed: false
+	}
+
+	await createNewTodo(newTodo)
+
+	getTodos()
+})
+
+
+/*************************************************************************************
+ * START
+ */
+
 
 getTodos()
