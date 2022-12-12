4300dbe816496dfd0e9152d45ed1119b801ba248
Kristopher Gray <kristopher.gray99@gmail.com>
2022-12-12 12:30:42 +0100

    toggle completed in server

--- 40-async-todos-ts/frontend/src/main.ts
+++ 40-async-todos-ts/frontend/src/main.ts
@@ -7,7 +7,7 @@ import './style.css'
  */
 
 interface ITodo {
-	id?: number,
+	id: number,
 	title: string,
 	completed: boolean
 }
@@ -26,8 +26,23 @@ let todos: ITodo[] = []
  */
 
 
-const checkRes = (res: Response) => {
-	if (!res.ok) throw new Error(`Could't fetch data, no respornse:, ${res.status} ${res.statusText}`)
+const changeTodoCompleted = async (e: Event) => {
+	const target = e.target as HTMLElement
+
+	if (target.tagName !== 'LI') {
+		return
+	}
+
+	const todoId = Number(target.dataset.todoId)
+	const clickedTodo: ITodo = todos.find(todo => todo.id === todoId) as ITodo
+	
+	await updateTodo(clickedTodo.id, {
+		completed: !clickedTodo.completed
+	})
+}
+
+const checkResponse = (res: Response) => {
+	if (!res.ok) throw new Error(`Could't fetch data, no response:, ${res.status} ${res.statusText}`)
 }
 
 const createNewTodo = async (newTodo: ITodo) => {
@@ -37,12 +52,12 @@ const createNewTodo = async (newTodo: ITodo) => {
 		body: JSON.stringify(newTodo),
 	})
 
-	checkRes(res)
+	checkResponse(res)
 }
 
 const fetchTodos = async () => {
 	const res = await fetch('http://localhost:3001/todos')
-	checkRes(res)
+	checkResponse(res)
 	return await res.json() as ITodo[]
 }
 
@@ -51,23 +66,36 @@ const getTodos = async () => {
 	renderTodos()
 }
 
+const refreshTodos = (el: Element, boo: Boolean) => {
+	el.innerHTML = ''
+	el.innerHTML = todos
+		.filter(todo => todo.completed === boo)
+		.map(todo => `
+			<li class="list-group-item" data-todo-id="${todo.id}">
+				${todo.title}
+			</li>`)
+		.join('')
+}
+
 const renderTodos = () => {
 	const todoListEl = document.querySelector('#todos')!
 	const completedTodoListEl = document.querySelector('#completed-todos')!
 
-	const refreshTodos = (el: Element, boo: Boolean) => {
-		el.innerHTML = ''
-		el.innerHTML = todos
-			.filter(todo => todo.completed === boo)
-			.map(todo => `
-				<li class="list-group-item data-todo-id="${todo.id}">
-					${todo.title}
-				</li>`)
-			.join('')
-	}
 	refreshTodos(todoListEl, false)
 	refreshTodos(completedTodoListEl, true)
 
+}
+
+const updateTodo = async (todoId: number, data: object) => {
+	const res = await fetch(`http://localhost:3001/todos/${todoId}`, {
+			method: 'PATCH',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(data),
+		})
+
+		checkResponse(res)
+
+		return await res.json();
 }
 
 
@@ -89,7 +117,11 @@ document.querySelector('#new-todo-form')?.addEventListener('submit', async e => 
 	const newTodoTitle = newTodoEl.value
 	newTodoEl.value = ''
 
+	// Find the highest value ID from the list items. Create a new ID that is 1 more
+    const newTodoId = Math.max(...todos.map(todo => todo.id)) + 1
+
 	const newTodo: ITodo = {
+		id: newTodoId,
 		title: newTodoTitle,
 		completed: false
 	}
@@ -99,6 +131,15 @@ document.querySelector('#new-todo-form')?.addEventListener('submit', async e => 
 	getTodos()
 })
 
+document.querySelector('#todos')?.addEventListener('click', async e => {
+	await changeTodoCompleted(e)
+	getTodos()
+})
+
+document.querySelector('#completed-todos')?.addEventListener('click', async e => {
+	await changeTodoCompleted(e)
+	getTodos()
+})
 
 /*************************************************************************************
  * START
